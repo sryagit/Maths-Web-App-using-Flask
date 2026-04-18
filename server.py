1663b32453cda3efee0b92f70afdcd1abd416a33
sryagit <158165193+sryagit@users.noreply.github.com>
2026-04-18 23:54:50 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from Maths.mathematics import summation, subtraction, multiplication
+
+app = Flask(__name__)
+
+@app.route("/sum")
+def sum_route():
+    num1 = float(request.args.get('num1')) 
+    num2 = float(request.args.get('num2')) 
+    result = summation(num1, num2) 
+    if result.is_integer():
+        result = int(result)
+    return str(result)
+
+@app.route("/sub")
+def sub_route():
+    num1 = float(request.args.get('num1')) 
+    num2 = float(request.args.get('num2')) 
+    result = subtraction(num1, num2)
+    if result.is_integer():
+        result = int(result)
+    return str(result)
+
+@app.route("/mul")
+def mul_route():
+    num1 = float(request.args.get('num1')) 
+    num2 = float(request.args.get('num2')) 
+    result = multiplication(num1, num2) 
+    if result.is_integer():
+        result = int(result)
+    return str(result)
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
